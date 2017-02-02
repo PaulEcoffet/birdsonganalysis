4972c75daf83e2b8726d11d9736a25a3470c0497
Paul Ecoffet <ecoffet.paul@gmail.com>
2017-02-02 17:38:07 +0100

    move test + begin fm function + wiener correction

--- birdsonganalysis/songanalysis.py
+++ birdsonganalysis/songanalysis.py
@@ -1,11 +1,25 @@
+"""
+Analysis of the song are taken from SAT: http://soundanalysispro.com/matlab-sat
+"""
+
 import numpy as np
+import libtfr
 from scipy.stats import gmean
 
-def wiener_entropy(signal):
-    return np.ln(gmean(signal)/np.mean(signal))
+def wiener_entropy(power, freq_range=1024):
+    # Taken from SAT
+    # Ignore low frequency power, starting only at the 10th element
+    sumlog = np.sum(np.log(power[9:freq_range, :]))
+    logsum = np.log(np.sum(power[9:freq_range, :])/(freq_range - 10))
+    return sumlog/(freq_range - 10) - logsum
 
 def song_similarity(song, reference_song):
     """
     Song similarity is asymmetric
     """
     raise NotImplemented
+
+def frequence_modulation(window):
+    D = libtfr.mfft_dpss(len(window), 1.5, 3)
+    Z = D.mtfft(window)
+    time_der = -Z[:]
